aa4360962e45a4fb3ed92f0117292d1238846dc1
Paula Sampaio <paulasampaio30ti@gmail.com>
2025-03-18 16:04:50 -0300

    Css

--- my--app/src/App.jsx
+++ my--app/src/App.jsx
@@ -8,28 +8,65 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+     <body class="fundo">
+    <div class="comteiner">
+    <div class="comteinerc">
+    
+    <div class="cabesalho">
+        <div class="divImg"><img class="logo" src="https://img.icons8.com/?size=100&id=rpU3BbTVADvt&format=png&color=000000"></div>
+        <div><p class="titulo">Pão De Mel  </p></div>
+    </div>
+    
+    </p>
+</div>
+</div>
+<div class="conteiner2">
+<div  class="topo " > <p>Não e so Chocolate e Uma  </p>
+<P>EXPERIENCIA</P></div>
 
-export default App
+
+
+
+
+
+</div>
+
+<div class="ali">
+    <div class="conteiner3 ">
+    
+   
+       <div class="gridItem caixa1 fontr"><p>DOCE DE LEITE </p></div>
+       <div class="gridItem caixa2  fontr"><p>DOCE DE LEIE</p></div>
+       <div class="gridItem  caixa3 fontr"> <p>BRIGADEIRO</p></div>
+       <div class="gridItem caixa4 fontr"><p>BRIGADEIRO</p></div>
+       
+       <div class="gridItem caixa5 fontr"><p >NINHO</p></div>
+       <div class="gridItem caixa6 new "> <P class="new1">NEW</P>
+    <p class=" cor">NINHO</p></div>
+       <div class="gridItem  caixa7 fontr"><p class="cor">MUSE DE LIMAO </p></div>
+       <div class="gridItem caixa8 new "> <P class="new1">NEW</P>
+        <p class=" cor">MUSE DE LIMAO</p></div>
+      
+       
+
+
+       
+      
+    </div>
+   
+    
+     </div>
+
+
+    
+
+
+<footer class="rodape "><div class="roda">
+    
+    <diV class="divImgr"> <img class="logor " src="https://img.icons8.com/?size=100&id=El7pTE24c3iC&format=png&color=000000"> <img class="logor " src=" https://img.icons8.com/?size=100&id=FeFKy6pAkhxv&format=png&color=000000"> <img class="logor " src=" https://img.icons8.com/?size=100&id=21088&format=png&color=000000"> <img class="logor " src="https://img.icons8.com/?size=100&id=33029&format=png&color=000000"></diV>    
+    </div>
+  <div>  <p>Pães de mel e cia MG/PR
+        </p><p> MG/ 9888898989</p></div>
+</div>
+</footer>
+</body>
